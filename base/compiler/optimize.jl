e2cc3f504e48cb5fcc4040c28598a8030093553f
Shuhei Kadowaki <aviatesk@gmail.com>
2022-02-03 19:35:46 +0900

    don't run `Local EA` for now

--- base/compiler/optimize.jl
+++ base/compiler/optimize.jl
@@ -552,7 +552,6 @@ function run_passes(ci::CodeInfo, sv::OptimizationState, caller::InferenceResult
     @timeit "Inlining"  ir = ssa_inlining_pass!(ir, ir.linetable, sv.inlining, ci.propagate_inbounds)
     # @timeit "verify 2" verify_ir(ir)
     @timeit "compact 2" ir = compact!(ir)
-    @timeit "Local EA"  state = analyze_escapes(ir, nargs, true, getter)
     @timeit "SROA"      ir = sroa_pass!(ir)
     @timeit "ADCE"      ir = adce_pass!(ir)
     @timeit "type lift" ir = type_lift_pass!(ir)
